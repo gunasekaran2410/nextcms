66b4b70d4d607751ef581c024eabe8198824f2b1
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-03-24 17:40:38 +0530

    update contents

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 // import Image from 'next/image'
 import Button from 'react-bootstrap/Button'
-// import ListGroup from 'react-bootstrap/ListGroup'
-// import Card from 'react-bootstrap/Card'
-// import Table from 'react-bootstrap/Table'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
 // import ClientSlide from '../components/ClientSlide';
 // import Footer from '../components/Footer'
 import { useRouter } from 'next/router'
@@ -56,7 +56,6 @@ export default function Home() {
                       </div>
                       <Button variant="primary" size="sm" className={styles.btn_sm}>
                       <span>Read more &nbsp;&nbsp;&nbsp; </span>
-                     
                       <FontAwesomeIcon className={styles.read_icon} icon={['fas', 'chevron-circle-right']} />
     </Button>
 
@@ -70,6 +69,127 @@ export default function Home() {
   
 </Container>
      </section>
+
+         
+{/* why us */}
+<section className={styles.light_gray_bg}>
+  
+
+  <Container>
+  <h3 className={styles.padding}> WHY US </h3>
+    
+    <div className={styles.text_center}>
+    <ListGroup horizontal style={{ background: 'transparent'}}>
+  
+    <ListGroup.Item>
+      <div className={styles.category}>
+      <FontAwesomeIcon className={styles.list_icon} icon={['fas', 'user-alt']} />
+      <p className={styles.paragraph}> Customer Focus</p>
+      </div>
+    </ListGroup.Item>
+  
+    <ListGroup.Item>
+      <div className={styles.category}>
+      <FontAwesomeIcon className={styles.list_icon} icon={['fas', 'trophy']} />
+      <p className={styles.paragraph}> Passion for Success</p>
+      </div>
+    </ListGroup.Item>
+  
+  
+    <ListGroup.Item>
+      <div className={styles.category}>
+      <FontAwesomeIcon className={styles.list_icon} icon={['fas', 'laptop']} />
+      <p className={styles.paragraph}> Technical Knowledge</p>
+      </div>
+    </ListGroup.Item>
+  
+    <ListGroup.Item>
+      <div className={styles.category}>
+      <FontAwesomeIcon className={styles.list_icon} icon={['fas', 'book']} />
+      <p className={styles.paragraph}>  Integrity & Accountability</p>
+      </div>
+    </ListGroup.Item>
+  
+    <ListGroup.Item>
+      <div className={styles.category}>
+      <FontAwesomeIcon className={styles.list_icon} icon={['fas', 'frown']} />
+      <p className={styles.paragraph}>   Excellence in Service </p>
+      </div>
+    </ListGroup.Item>
+  
+  
+  </ListGroup>
+    </div>
+  <br></br><br></br>
+    <h3 className={styles.padding}> OUR MANTRA </h3>
+  <Row>
+  <Card className={styles.margin_10}>
+    <Card.Body className={styles.padding_10}>
+    <Table className={styles.table}>
+    <tbody>
+      <tr>
+        <td className={styles.border_none}>
+          {/* <Image width="60" height="60" src="/images/innovate.svg"/> */}
+        </td>
+        <td className={styles.border_none}>
+          <h4>INNOVATE</h4>
+          <p className={styles.paragraph}>
+          Innovation is the ability to see change as an opportunity that unlocks new value. There is a way to do it better – we are finding it.s
+          </p>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
+    </Card.Body>
+  </Card>
+  
+  <Card className={styles.margin_10}>
+    <Card.Body className={styles.padding_10}>
+    <Table className={styles.table}>
+    <tbody>
+      <tr>
+        <td className={styles.border_none}>
+          {/* <Image width="80" height="80" src="/images/implementation.svg"/> */}
+        </td>
+        <td className={styles.border_none}>
+          <h4>IMPLEMENT</h4>
+          <p className={styles.paragraph}>
+          An Innovation idea needs the right implementation to find success. It is not just the action, but right action determines the product fate. We focus on the right way of building the product with the right technology.
+          </p>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
+    </Card.Body>
+  </Card>
+  
+  <Card className={styles.margin_10}>
+    <Card.Body className={styles.padding_none}>
+    <Table className={styles.table}>
+    <tbody>
+      <tr>
+        <td className={styles.border_none}>
+          {/* <Image width="120" height="120" src="/images/iteration.svg"/> */}
+        </td>
+        <td className={styles.border_none}>
+          <h4>ITERATE</h4>
+          <p className={styles.paragraph}>
+          Change is the only thing that is constant. A product that does not iterate has an expiry date tagged to it as soon as it comes out. Through an iterative process, we can keep the original focus, validating for current situations, helping us keep the ideas fresh and products competitive.
+          </p>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
+    </Card.Body>
+  </Card>
+  </Row>
+  
+  </Container>
+  
+  <br/>
+  <br/>
+  
+  </section>
      
 
 
