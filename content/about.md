4ad3c841d9c8e706042cd034604dd791f126e0d1
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-03-27 15:32:12 +0530

    file update

--- content/about.md
+++ content/about.md
@@ -1,10 +1,24 @@
 ---
-title: A GREAT COMPANY STARTS WITH US
+title: OUR MANTRA
 sectionTitle: MISSION & VISION(COMMITTED TO EXCELLENCE)
-date: 2019-03-21T19:31:20.591Z
-description: sadsddaddsd
-about_us:
-  - name: Product Development1
-    mission_image: images/mission.png
-    description: " Make Technology an asset for your business 2"
+abouts:
+  - description: >
+      Innovation is the ability to see change as an opportunity that unlocks new
+      value. There is a way to do it better – we are finding it.
+    name: INNOVATE
+    mission_image: images/innovate.svg
+  - description: An Innovation idea needs the right implementation to find success.
+      It is not just the action, but right action determines the product fate.
+      We focus on the right way of building the product with the right
+      technology.
+    name: IMPLEMENT
+    mission_image: images/implementation.svg
+  - description: Change is the only thing that is constant. A product that does not
+      iterate has an expiry date tagged to it as soon as it comes out. Through
+      an iterative process, we can keep the original focus, validating for
+      current situations, helping us keep the ideas fresh and products
+      competitive.
+    name: ITERATE
+    mission_image: images/iteration.svg
 ---
+ 
--- pages/About.js
+++ pages/About.js
@@ -9,20 +9,20 @@ import { attributes, react as AboutContent } from '../content/about.md';
 
 
 function About() {
-    let { title, sectionTitle, about_us } = attributes;
+    let { title, sectionTitle, abouts } = attributes;
     return <div>
           <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
         <Navigation/>
-     
+        <AboutContent/>
         <section className={styles.sub_bnr}>
         <h1> {title}</h1>
         </section>
 
 {/* mission & vision  */}
 <div>
-          <AboutContent/>
+         
           <br/>
 <h3 className={styles.padding}> 
     {sectionTitle}
@@ -32,18 +32,18 @@ function About() {
 <section>
 <Container>
 <Row>
-{about_us.map((about_list, k) => (
+{abouts.map((about, k) => (
 
     <Col md={3} key={k} >
         <div className={styles.mission_box}>
         <div className={styles.mission_box_title}>
-         {about_list.name}
+         {about.name}
  </div>
 <div className={styles.mission_card_body}>
-    <img height="96" width="96"  src={about_list.mission_image} alt='about image' />
+    <img height="96" width="96"  src={about.mission_image} alt='about image' />
 </div>
 <div className={styles.mission_content}>
-    {about_list.description}
+    {about.description}
     </div>
 
         </div>
