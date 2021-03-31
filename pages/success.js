73cbb2064b0795c26b534214879e8ca95f8fdb9d
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-03-31 17:22:47 +0530

    success msg add

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,12 @@
+import Card from 'react-bootstrap/Card'
+import styles from './Index.module.css'
 export default function Success() {
-    return <div>Form successfully submitted!</div>
+    return <div className={styles.text_center}>
+        <Card>
+            <Card.Body>
+                <img src="/images/ok.gif" alt=""/>
+            <h1>Your Application was successfully submitted!</h1>
+            </Card.Body>
+        </Card>
+    </div>
   }
