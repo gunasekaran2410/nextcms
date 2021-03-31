781e201fc3bfb32a9967bf4bb5d880799b138b3b
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-03-31 12:27:02 +0530

    id add

--- components/Forms.js
+++ components/Forms.js
@@ -64,19 +64,14 @@ function Forms() {
 
           <form name="contact" method="POST" data-netlify="true">
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
+    <label>Your Name: <input type="text" id="name" name="name" /></label>   
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
+    <label>Your Email: <input type="email" id="email" name="email" /></label>
   </p>
+ 
   <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
+    <label>Message: <textarea name="message" id="message"></textarea></label>
   </p>
   <p>
     <button type="submit">Send</button>
