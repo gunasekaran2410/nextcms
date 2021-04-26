6901c869629e1ee97a55350ef5de1768d4efe991
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-04-26 13:30:50 +0530

    default home load add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/Home',
+            permanent: true,
+          },
+        ]
+      },
     trailingSlash: true,
     env: {
         dev_asset:"",
